0c524407479365c2cb36215c45d53bcb1c8b128b
Rob Paik <robpaik@gmail.com>
2019-07-23 19:21:15 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -51,7 +51,7 @@ def run
   help
   puts "Please enter a command:"
   input = gets.chomp
-  while input != "exit"
+  while input != "exit" do
     if input == "help"
       help
       puts "Please enter a command:"
